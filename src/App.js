22f7fb11c55781572b7ac2f0a6c15e9f44331778
Erhan Sönmez <erhansonmez@outlook.com>
2019-08-05 15:01:43 +0300

    Sayfa Geliştirmeleri
    Film js componenti ve 404 sayfası eklendi.
    
    css düzenlemeleri ve anasayfa kod geliştirmeleri yapıldı.
    
    Apollo componenti kullanılarak veriler uzak suncudan çekildi ve buna göre geliştirmeler yapıldı.

--- src/App.js
+++ src/App.js
@@ -9,28 +9,29 @@ import Filmler from "./pages/Filmler";
 import Oyuncular from "./pages/Oyuncular";
 import Film from "./pages/Film";
 import Oyuncu from "./pages/Oyuncu";
+import Error_404 from "./pages/Error_404";
 
 class App extends React.Component{
 
-  render() {
+   render() {
     return (
+      <div className="wrapper">
       <Router>
-        <div className="menu">
-          <h1>Menüler</h1>
-          {/* Rota Bileşenleri */}
-          <Route component={Anasayfa} path="/" exact />
-          <Route component={Filmler} path="/filmler" exact />
-          <Route component={Oyuncular} path="/oyuncular" exact />
-          <Route component={Film} path="/filmler/:filmId" />
-          <Route component={Oyuncu} path="/oyuncular/:oyuncuId" />
-          {/* Rota Linkleri */}
-          <div className="nav">
-            <Link to="/">Anasayfa</Link>
-            <Link to="/filmler">Filmler</Link>
-            <Link to="/oyuncular">Oyuncular</Link>
-          </div>
+        {/* Rota Linkleri */}
+        <div className="nav">
+          <Link to="/">Anasayfa</Link>
+          <Link to="/filmler">Filmler</Link>
+          <Link to="/oyuncular">Oyuncular</Link>
         </div>
+        {/* Rota Bileşenleri */}
+        <Route component={Anasayfa} path="/" exact />
+        <Route component={Filmler} path="/filmler" exact />
+        <Route component={Oyuncular} path="/oyuncular" exact />
+        <Route component={Film} path="/filmler/:filmId" />
+        <Route component={Oyuncu} path="/oyuncular/:oyuncuId" />
+        <Route component={Error_404} />
       </Router>
+      </div>
     );
   }
 }
---
+++ src/pages/Error_404.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Error_404 extends React.Component{
+
+  render() {
+    return (
+      "Sayfa Bulunamadı"
+    );
+  }
+}
+
+export default Error_404;
